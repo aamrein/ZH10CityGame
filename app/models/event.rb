649b185da3e1e01f34740c102617df5825b0e48f
Andreas Amrein <aamrein1@gmail.com>
2018-04-05 22:31:32 +0200

    validate if event fields are present

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,8 @@
 class Event < ApplicationRecord
+  validates :name, :presence => true
+  validates :impact_percent, :presence => true
+  validates :duration_sec, :presence => true
+
   def duration_min
     self.duration_sec / 60
   end
