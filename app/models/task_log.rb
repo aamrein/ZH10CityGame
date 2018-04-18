db7b3434d66afb5ecb7b52be3b5c11d86fd68705
Andreas Amrein <aamrein1@gmail.com>
2018-04-18 07:41:59 +0200

    show remaining time on task logs

--- app/models/task_log.rb
+++ app/models/task_log.rb
@@ -5,4 +5,16 @@ class TaskLog < ApplicationRecord
     !self.start.nil?
   end
 
+  def on_time
+    self.start.getutc + self.task.duration_min * 60 > Time.now.getutc
+  end
+
+  def remaining_time
+    remaining_time = self.task.duration_min
+    if self.started
+      remaining_time = (self.start.getutc + self.task.duration_min * 60) - DateTime.now.getutc
+    end
+    remaining_time > 0 ? remaining_time : 0
+  end
+
 end
