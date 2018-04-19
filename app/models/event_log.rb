37db970ee7b5be0de6c96ccbc664d29f58da4f77
Andreas Amrein <aamrein1@gmail.com>
2018-04-19 15:00:10 +0200

    change event log view for players

--- app/models/event_log.rb
+++ app/models/event_log.rb
@@ -1,4 +1,18 @@
 class EventLog < ApplicationRecord
   belongs_to :event
   belongs_to :constructed_building
+
+  def end
+    self.start.advance(minutes: self.event.duration_min)
+  end
+
+  def remaining_time
+    remaining_time = (self.start.getutc + self.event.duration_min * 60) - DateTime.now.getutc
+    remaining_time > 0 ? remaining_time : 0
+  end
+
+  def remaining_impact
+    remaining_impact = self.event.impact_percent * (self.remaining_time / (self.event.duration_min * 60))
+    remaining_impact > 0 ? remaining_impact : 0
+  end
 end
